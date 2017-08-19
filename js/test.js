346e3e6729d2d8c40281a722ff566a24c7568be9
oni <stevoni>
2017-08-19 03:33:28 +0100

    First commit

---
+++ js/test.js
@@ -0,0 +1,309 @@
+var classes_txt = ['airplane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'];
+var maxmin = cnnutil.maxmin;
+var f2t = cnnutil.f2t;
+var draw_activations = function(elt, A, scale, grads) {
+
+  var s = scale || 2; // scale
+  var draw_grads = false;
+  if(typeof(grads) !== 'undefined') draw_grads = grads;
+
+  // get max and min activation to scale the maps automatically
+  var w = draw_grads ? A.dw : A.w;
+  var mm = maxmin(w);
+
+  // create the canvas elements, draw and add to DOM
+  for(var d=0;d<A.depth;d++) {
+
+    var canv = document.createElement('canvas');
+    canv.className = 'actmap';
+    var W = A.sx * s;
+    var H = A.sy * s;
+    canv.width = W;
+    canv.height = H;
+    var ctx = canv.getContext('2d');
+    var g = ctx.createImageData(W, H);
+
+    for(var x=0;x<A.sx;x++) {
+      for(var y=0;y<A.sy;y++) {
+        if(draw_grads) {
+          var dval = Math.floor((A.get_grad(x,y,d)-mm.minv)/mm.dv*255);
+        } else {
+          var dval = Math.floor((A.get(x,y,d)-mm.minv)/mm.dv*255);  
+        }
+        for(var dx=0;dx<s;dx++) {
+          for(var dy=0;dy<s;dy++) {
+            var pp = ((W * (y*s+dy)) + (dx + x*s)) * 4;
+            for(var i=0;i<3;i++) { g.data[pp + i] = dval; } // rgb
+            g.data[pp+3] = 255; // alpha channel
+          }
+        }
+      }
+    }
+    ctx.putImageData(g, 0, 0);
+    elt.appendChild(canv);
+  }  
+}
+var draw_activations_COLOR = function(elt, A, scale, grads) {
+
+  var s = scale || 2; // scale
+  var draw_grads = false;
+  if(typeof(grads) !== 'undefined') draw_grads = grads;
+
+  // get max and min activation to scale the maps automatically
+  var w = draw_grads ? A.dw : A.w;
+  var mm = maxmin(w);
+
+  var canv = document.createElement('canvas');
+  canv.className = 'actmap';
+  var W = A.sx * s;
+  var H = A.sy * s;
+  canv.width = W;
+  canv.height = H;
+  var ctx = canv.getContext('2d');
+  var g = ctx.createImageData(W, H);
+  for(var d=0;d<3;d++) {
+    for(var x=0;x<A.sx;x++) {
+      for(var y=0;y<A.sy;y++) {
+        if(draw_grads) {
+          var dval = Math.floor((A.get_grad(x,y,d)-mm.minv)/mm.dv*255);
+        } else {
+          var dval = Math.floor((A.get(x,y,d)-mm.minv)/mm.dv*255);  
+        }
+        for(var dx=0;dx<s;dx++) {
+          for(var dy=0;dy<s;dy++) {
+            var pp = ((W * (y*s+dy)) + (dx + x*s)) * 4;
+            g.data[pp + d] = dval;
+            if(d===0) g.data[pp+3] = 255; // alpha channel
+          }
+        }
+      }
+    }
+  }
+  ctx.putImageData(g, 0, 0);
+  elt.appendChild(canv);
+}
+var visualize_activations = function(net, elt) {
+
+  // clear the element
+  elt.innerHTML = "";
+
+  // show activations in each layer
+  var N = net.layers.length;
+  for(var i=0;i<N;i++) {
+    var L = net.layers[i];
+
+    var layer_div = document.createElement('div');
+
+    // visualize activations
+    var activations_div = document.createElement('div');
+    activations_div.appendChild(document.createTextNode('Activations:'));
+    activations_div.appendChild(document.createElement('br'));
+    activations_div.className = 'layer_act';
+    var scale = 2;
+    if(L.layer_type==='softmax' || L.layer_type==='fc') scale = 10; // for softmax
+    
+    // HACK to draw in color in input layer
+    if(i===0) {
+      draw_activations_COLOR(activations_div, L.out_act, scale);
+      draw_activations_COLOR(activations_div, L.out_act, scale, true);
+
+      /*
+      // visualize positive and negative components of the gradient separately
+      var dd = L.out_act.clone();
+      var ni = L.out_act.w.length;
+      for(var q=0;q<ni;q++) { var dwq = L.out_act.dw[q]; dd.w[q] = dwq > 0 ? dwq : 0.0; }
+      draw_activations_COLOR(activations_div, dd, scale);
+      for(var q=0;q<ni;q++) { var dwq = L.out_act.dw[q]; dd.w[q] = dwq < 0 ? -dwq : 0.0; }
+      draw_activations_COLOR(activations_div, dd, scale);
+      */
+
+      /*
+      // visualize what the network would like the image to look like more
+      var dd = L.out_act.clone();
+      var ni = L.out_act.w.length;
+      for(var q=0;q<ni;q++) { var dwq = L.out_act.dw[q]; dd.w[q] -= 20*dwq; }
+      draw_activations_COLOR(activations_div, dd, scale);
+      */
+
+      /*
+      // visualize gradient magnitude
+      var dd = L.out_act.clone();
+      var ni = L.out_act.w.length;
+      for(var q=0;q<ni;q++) { var dwq = L.out_act.dw[q]; dd.w[q] = dwq*dwq; }
+      draw_activations_COLOR(activations_div, dd, scale);
+      */
+
+    } else {
+      draw_activations(activations_div, L.out_act, scale);
+    } 
+
+    // visualize data gradients
+    if(L.layer_type !== 'softmax' && L.layer_type !== 'input' ) {
+      var grad_div = document.createElement('div');
+      grad_div.appendChild(document.createTextNode('Activation Gradients:'));
+      grad_div.appendChild(document.createElement('br'));
+      grad_div.className = 'layer_grad';
+      var scale = 2;
+      if(L.layer_type==='softmax' || L.layer_type==='fc') scale = 10; // for softmax
+      draw_activations(grad_div, L.out_act, scale, true);
+      activations_div.appendChild(grad_div);
+    }
+
+    // visualize filters if they are of reasonable size
+    if(L.layer_type === 'conv') {
+      var filters_div = document.createElement('div');
+      if(L.filters[0].sx>3) {
+        // actual weights
+        filters_div.appendChild(document.createTextNode('Weights:'));
+        filters_div.appendChild(document.createElement('br'));
+        for(var j=0;j<L.filters.length;j++) {
+          // HACK to draw in color for first layer conv filters
+          if(i===1) {
+            draw_activations_COLOR(filters_div, L.filters[j], 2);
+          } else {
+            filters_div.appendChild(document.createTextNode('('));
+            draw_activations(filters_div, L.filters[j], 2);
+            filters_div.appendChild(document.createTextNode(')'));
+          }
+        }
+        // gradients
+        filters_div.appendChild(document.createElement('br'));
+        filters_div.appendChild(document.createTextNode('Weight Gradients:'));
+        filters_div.appendChild(document.createElement('br'));
+        for(var j=0;j<L.filters.length;j++) {
+          if(i===1) { draw_activations_COLOR(filters_div, L.filters[j], 2, true); }
+          else {
+            filters_div.appendChild(document.createTextNode('('));
+            draw_activations(filters_div, L.filters[j], 2, true);
+            filters_div.appendChild(document.createTextNode(')'));
+          }
+        }
+      } else {
+        filters_div.appendChild(document.createTextNode('Weights hidden, too small'));
+      }
+      activations_div.appendChild(filters_div);
+    }
+    layer_div.appendChild(activations_div);
+
+    // print some stats on left of the layer
+    layer_div.className = 'layer ' + 'lt' + L.layer_type;
+    var title_div = document.createElement('div');
+    title_div.className = 'ltitle'
+    var t = L.layer_type + ' (' + L.out_sx + 'x' + L.out_sy + 'x' + L.out_depth + ')';
+    title_div.appendChild(document.createTextNode(t));
+    layer_div.appendChild(title_div);
+
+    if(L.layer_type==='conv') {
+      var t = 'filter size ' + L.filters[0].sx + 'x' + L.filters[0].sy + 'x' + L.filters[0].depth + ', stride ' + L.stride;
+      layer_div.appendChild(document.createTextNode(t));
+      layer_div.appendChild(document.createElement('br'));
+    }
+    if(L.layer_type==='pool') {
+      var t = 'pooling size ' + L.sx + 'x' + L.sy + ', stride ' + L.stride;
+      layer_div.appendChild(document.createTextNode(t));
+      layer_div.appendChild(document.createElement('br'));
+    }
+
+    // find min, max activations and display them
+    var mma = maxmin(L.out_act.w);
+    var t = 'max activation: ' + f2t(mma.maxv) + ', min: ' + f2t(mma.minv);
+    layer_div.appendChild(document.createTextNode(t));
+    layer_div.appendChild(document.createElement('br'));
+
+    var mma = maxmin(L.out_act.dw);
+    var t = 'max gradient: ' + f2t(mma.maxv) + ', min: ' + f2t(mma.minv);
+    layer_div.appendChild(document.createTextNode(t));
+    layer_div.appendChild(document.createElement('br'));
+
+    // number of parameters
+    if(L.layer_type==='conv' || L.layer_type==='local') {
+      var tot_params = L.sx*L.sy*L.in_depth*L.filters.length + L.filters.length;
+      var t = 'parameters: ' + L.filters.length + 'x' + L.sx + 'x' + L.sy + 'x' + L.in_depth + '+' + L.filters.length + ' = ' + tot_params;
+      layer_div.appendChild(document.createTextNode(t));
+      layer_div.appendChild(document.createElement('br'));
+    }
+    if(L.layer_type==='fc') {
+      var tot_params = L.num_inputs*L.filters.length + L.filters.length;
+      var t = 'parameters: ' + L.filters.length + 'x' + L.num_inputs + '+' + L.filters.length + ' = ' + tot_params;
+      layer_div.appendChild(document.createTextNode(t));
+      layer_div.appendChild(document.createElement('br'));
+    }
+
+    // css madness needed here...
+    var clear = document.createElement('div');
+    clear.className = 'clear';
+    layer_div.appendChild(clear);
+
+    elt.appendChild(layer_div);
+  }
+}
+
+function testImage(image,val){
+    var d = data;
+    net = new convnetjs.Net();
+    net.fromJSON(d);
+    if(val==='yes'){
+      imagetet(image);
+    }
+    else{
+      notest(image);
+    }
+    
+}
+function imagetet(img){
+    var x = convnetjs.img_to_vol(img);
+    var out_p = net.forward(x);
+
+    var vis_elt = document.getElementById("visnet");
+    visualize_activations(net, vis_elt);
+
+    var preds =[]
+    for(var k=0;k<out_p.w.length;k++) { preds.push({k:k,p:out_p.w[k]}); }
+    preds.sort(function(a,b){return a.p<b.p ? 1:-1;});
+
+    // add predictions
+    var div = document.getElementById('div1');
+    div.className = 'testdiv';
+
+    draw_activations_COLOR(div, x, 2);
+
+    var probsdiv = document.createElement('div');
+
+    var t = '';
+    for(var k=0;k<3;k++) {
+      var col = k===0 ? 'rgb(85,187,85)' : 'rgb(187,85,85)';
+      t += '<div class=\"pp\" style=\"width:' + Math.floor(preds[k].p/1*100) + 'px; background-color:' + col + ';\">' + classes_txt[preds[k].k] + '</div>';
+      console.log(classes_txt[preds[k].k]);
+    }
+    probsdiv.innerHTML = t;
+    probsdiv.className = 'probsdiv';
+    div.appendChild(probsdiv);
+
+    // add it into DOM
+    $(div).prependTo($("#testset_vis")).hide().fadeIn('slow').slideDown('slow');
+    if($(".probsdiv").length>200) {
+      $("#testset_vis > .probsdiv").last().remove(); // pop to keep upper bound of shown items
+    }
+}
+
+function notest(img){
+  var x = convnetjs.img_to_vol(img);
+  var out_p = net.forward(x);
+
+  var preds = [];
+  for(var k=0;k<out_p.w.length;k++) { preds.push({k:k,p:out_p.w[k]}); }
+  preds.sort(function(a,b){return a.p<b.p ? 1:-1;});
+  
+  var div = document.getElementById('visn');
+
+  var probsdiv = document.createElement('div');
+  var t = '';
+  for(var k=0;k<3;k++) {
+      var col = k===0 ? 'rgb(85,187,85)' : 'rgb(187,85,85)';
+      t += '<div class=\"pp\" style=\"width:' + Math.floor(preds[k].p/1*100) + 'px; background-color:' + col + ';\">' + classes_txt[preds[k].k] + '</div>';
+      console.log(classes_txt[preds[k].k]);
+    }
+  probsdiv.innerHTML = t;
+  probsdiv.className = 'probsdiv';
+  div.appendChild(probsdiv);
+}
